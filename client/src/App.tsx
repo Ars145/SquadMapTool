5ae6c43ab16a11ebb22a32b6b02051ce2bc57c8f
Arseny <arscyg.tsygankov@mail.ru>
2026-04-21 17:29:19 +0300

    Fix Pages routing with base URL and replace favicon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import { useEffect } from "react";
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import MapEditor from "@/pages/map-editor";
 
+const base = import.meta.env.BASE_URL.replace(/\/$/, "");
+
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={MapEditor} />
-    </Switch>
+    <WouterRouter base={base}>
+      <Switch>
+        <Route path="/" component={MapEditor} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
